5aedbf51327ef4b39f558dd90e00ca0434d9adaa
Amoxe <amoxe@DESKTOP-V5KGI93.localdomain>
2022-09-28 19:14:38 +0100

    parsing started and root level in the config file

---
+++ root.hpp
@@ -0,0 +1,33 @@
+#ifndef ROOT_HPP
+#define ROOT_HPP
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include "server.hpp"
+
+class Root {
+
+private:
+std::vector<Server> servers;
+
+
+protected:
+std::string index;
+std::string i_val;
+std::string body_size;
+std::string size;
+std::string allowed_mtd;
+std::string type_mtd;
+std::string auto_index;
+std::string status;
+
+
+public:
+    Root();
+    ~Root();
+    std::map<std::string, std::string> elements;
+};
+
+
+#endif
